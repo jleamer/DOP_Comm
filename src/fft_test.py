95086915e4cbe3f71b816bd1b1e435770df39763
jleamer <jleamer@tulane.edu>
2022-04-21 11:50:14 -0500

    Added jupyter notebook file for multidimensional fft testing.  The Gaussian examples from fft_test.py have been moved to the notebook.  Now fft_test.py only includes the examples from Tatarski's book

--- src/fft_test.py
+++ src/fft_test.py
@@ -2,55 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import fft
 
-
-def fft_3D(f_vals, i_grid, j_grid, k_grid, di, dj, dk):
-    result = np.zeros_like(f_vals, dtype=complex)
-    i_minus = (-1) ** np.arange(i_grid)
-    j_minus = (-1) ** np.arange(j_grid)
-    k_minus = (-1) ** np.arange(k_grid)
-
-    for j in range(j_grid):
-        for k in range(k_grid):
-            result[:, j, k] += i_minus * fft.fft(i_minus * f_vals[:, j, k]) * di / (2 * np.pi)
-
-    for i in range(i_grid):
-        for k in range(k_grid):
-            result[i, :, k] += j_minus * fft.fft(j_minus * f_vals[i, :, k]) * dj / (2 * np.pi)
-
-    for i in range(i_grid):
-        for j in range(j_grid):
-            result[i, j, :] += k_minus * fft.fft(k_minus * f_vals[i, j, :]) * dk / (2 * np.pi)
-
-    return result / (2 * np.pi) ** 2
-
-
-def fft_2D(f_vals, x_grid, y_grid, delta_x, delta_y):
-    i_minus = (-1) ** np.arange(x_grid)
-
-    x_minus = np.array([i_minus for _ in range(x_grid)])
-    minus = np.array([[(-1) ** (i+j) for i in range(x_grid)] for j in range(y_grid)])
-
-    result = x_minus * fft.fft(minus * fft.fft(x_minus.T * f_vals, axis=0) * delta_x / np.sqrt(2 * np.pi), axis=1) * delta_y / np.sqrt(2 * np.pi)
-    #result = minus * fft.fft2(minus * f_vals) * delta_x * delta_y / (2 * np.pi)
-
-
-    """
-    for i in range(x_grid):
-        result[i, :] = minus[i, :] * fft.fft(minus[i, :] * f_vals[i, :]) * delta_y / np.sqrt(2 * np.pi)
-
-    for j in range(y_grid):
-        result[:, j] = minus[:, j] * fft.fft(minus[:, j] * result[:, j]) * delta_x / np.sqrt(2 * np.pi)
-    """
-    """
-    temp = np.zeros_like(result, dtype=complex)
-    for i in range(x_grid):
-        for j in range(y_grid):
-            temp = j_minus * fft.fft((-1) ** (i + j) * fft.fft(i_minus * f_vals[i, j]) * delta_x) * delta_y
-    """
-
-    #result = ii * fft.fft2(jj * f_vals) * delta_x * delta_y
-    return result
-
 #########################################
 #
 # 1D Tatarski example
@@ -67,7 +18,7 @@ index = np.arange(x_grid_dim)
 x = (index - x_grid_dim / 2) * dx
 
 # Get k space grid
-k = (index - x_grid_dim / 2) * (np.pi / x_amplitude)
+kx = (index - x_grid_dim / 2) * (np.pi / x_amplitude)
 
 # Set up minus array
 minus = (-1) ** np.arange(x_grid_dim)
@@ -78,12 +29,12 @@ spec_dens = minus * fft.fft(minus * analytic_corr) * dx / (2 * np.pi)
 
 # Define analtyic spectral density and calculate corr
 #analytic_spec_dens = a ** 2 * r0 ** 3 / (np.pi ** 2 * (1 + r0 ** 2 * k ** 2) ** 2)
-analytic_spec_dens = a ** 2 * r0 / (np.pi * (1 + k ** 2 * r0 ** 2))
+analytic_spec_dens = a ** 2 * r0 / (np.pi * (1 + kx ** 2 * r0 ** 2))
 corr = minus * fft.ifft(minus * analytic_spec_dens) / dx * 2 * np.pi
 
 plt.figure(1)
-plt.plot(k, spec_dens, label='From FFT')
-plt.plot(k, analytic_spec_dens, label='From Eqn')
+plt.plot(kx, spec_dens, label='From FFT')
+plt.plot(kx, analytic_spec_dens, label='From Eqn')
 plt.ylabel("phi(k)")
 plt.xlabel("k")
 plt.legend(numpoints=1)
@@ -97,121 +48,51 @@ plt.legend(numpoints=1)
 
 ########################################################
 #
-# Try same approach for a known 2D function
+# Try same approach for a known 3D function from Tatarski
 #
 ########################################################
-y_amplitude = 10
-y_grid_dim = 256
-dy = 2 * y_amplitude / y_grid_dim
-y_index = np.arange(y_grid_dim)
-y = (y_index - y_grid_dim / 2) * dy
-ky = (index - y_grid_dim / 2) * (np.pi / y_amplitude)
 
-# Define mesh grid for (x, y) and (kx, ky)
-X, Y = np.meshgrid(x, y)
-Kx, Ky = np.meshgrid(k, ky)
+# Define grid terms for z
+z_amplitude = 10
+z_grid_dim = 256
+dz = 2 * z_amplitude / z_grid_dim
+z_index = np.arange(z_grid_dim)
+z = (z_index - z_grid_dim / 2) * dz
+kz = (index - z_grid_dim / 2) * (np.pi / z_amplitude)
 
-#print(np.allclose(Kx, 2*np.pi*X/(x_grid_dim * dx ** 2)))
+# Define mesh grid for space and spatial frequency
+X, Y, Z = np.meshgrid(x, y, z)
+Kx, Ky, Kz = np.meshgrid(kx, ky, kz)
 
 # Define analytic functions
-analytic_f = np.exp(-np.pi * (X ** 2 + Y ** 2))
-analytic_g = 1/(2*np.pi) * np.exp(-1/(4*np.pi) * (Kx ** 2 + Ky ** 2))
-
-# Attempt to calculate Fourier transform of each
-#g = fft_2D(analytic_f, x_grid_dim, y_grid_dim, dx, dy)
-minus_mk = np.array([[(-1) ** (i+j) for i in range(x_grid_dim)] for j in range(y_grid_dim)])
-minus_l = np.array([minus for _ in range(x_grid_dim)])
-g = minus_l * fft.fft(minus_mk * fft.fft(minus_l.T * analytic_f, axis=0) * dx / np.sqrt(2 * np.pi), axis=1) * dy / np.sqrt(2 * np.pi)
-print("Analytic g = Calculated g?: ", np.allclose(analytic_g, g.real))
-
-fig3 = plt.figure(3)
-ax3 = fig3.add_subplot(projection='3d')
-ax3.plot_wireframe(Kx, Ky, analytic_g, label='Analytic')
-ax3.plot_wireframe(Kx, Ky, g, label='FFT', color="orange")
-ax3.set_xlabel("Kx")
-ax3.set_ylabel("Ky")
-ax3.set_zlabel("g(kx, ky)")
-ax3.legend(numpoints=1)
-
-# Attempt to calculate the inverse Fourier transform of each
-f = minus_l * fft.ifft(minus_mk * fft.ifft(minus_l.T * analytic_g, axis=0) / dx * np.sqrt(2*np.pi), axis=1) / dy * np.sqrt(2*np.pi)
-print("Analytic f = Calculated f?: ", np.allclose(analytic_f, f.real))
-
-fig4 = plt.figure(4)
-ax4 = fig4.add_subplot(projection='3d')
-ax4.plot_wireframe(X, Y, analytic_f, label='Analytic')
-ax4.plot_wireframe(X, Y, f, color='orange', label='iFFT')
-ax4.set_xlabel("X")
-ax4.set_ylabel("Y")
-ax4.set_zlabel("f(x, y)")
-ax4.legend(numpoints=1)
-
-"""
-# Try 3D case now
-# Define y and z grids
-y_grid_dim = 256
-z_grid_dim = 256
-
-y_amplitude = 10
-z_amplitude = 10
-
-dy = 2 * y_amplitude / y_grid_dim
-dz = 2 * z_amplitude / z_grid_dim
-
-y_index = np.arange(y_grid_dim)
-z_index = np.arange(z_grid_dim)
-
-y = (y_index - y_grid_dim / 2) * dy
-z = (z_index - z_grid_dim / 2) * dz
-
-# Create k_y and k_z values
-k_y = (y_index - y_grid_dim / 2) * (np.pi / y_amplitude)
-k_z = (z_index - z_grid_dim / 2) * (np.pi / z_amplitude)
-
-# Create 3D mesh grid for calculating functions
-xx, yy, zz = np.meshgrid(x, y, z)
-kkx, kky, kkz = np.meshgrid(k, k_y, k_z)
-
-# Calculate analytical correlation function
-analytic_corr_3D = a ** 2 * np.exp(-np.abs(np.sqrt(xx**2 + yy**2 + zz**2)/r0))
+R = np.sqrt(X ** 2 + Y ** 2 + Z ** 2)
+analytic_corr_3D = a ** 2 * np.exp(-np.abs(R/r0))
+K = np.sqrt(Kx ** 2 + Ky ** 2 + Kz ** 2)
+#analytic_spec_dens_3D = a ** 2 * r0 ** 3 / (np.pi ** 2 * (1 + r0 ** 2 * K ** 2) ** 2)
+analytic_spec_dens_3D = a ** 2 * r0 / (np.pi * (1 + K ** 2 * r0 ** 2))
 
 
-plt.figure(4)
-plt.plot(xx[128, :, 128], label='x')
-plt.plot(yy[:, 128, 128], label='y')
-plt.plot(zz[128, 128, :], label='z')
-plt.legend(numpoints=1)
+# Create arrays for minus terms
+minus_x = np.array([[[(-1) ** i for k in range(z_grid_dim)] for j in range(y_grid_dim)] for i in range(x_grid_dim)])
+minus_y = minus_x.transpose((1, 0, 2)).copy()
+minus_z = minus_x.transpose((2, 1, 0)).copy()
 
-plt.figure(5)
-#plt.imshow(xx[:, :, 128], origin='lower')
-#plt.imshow(yy[:, :, 128], origin='lower')
-plt.imshow(zz[128, :, :], origin='lower')
+# Calculate FFT and iFFT
+# Does not work as written - need to figure out how to handle minuses properly
+spec_dens_3D = minus_x * fft.fft(minus_x * minus_y * fft.fft(minus_y * minus_z * fft.fft(minus_z * analytic_corr_3D, axis=2) * dx / np.sqrt(2*np.pi), axis=1) * dy / np.sqrt(2*np.pi), axis=0) * dz / np.sqrt(2*np.pi)
+corr_3D = minus_x * fft.ifft(minus_x * minus_y * fft.ifft(minus_y * minus_z * fft.ifft(minus_z * analytic_spec_dens_3D, axis=2) / dx * np.sqrt(2*np.pi), axis=1) / dy * np.sqrt(2*np.pi), axis=0) / dz * np.sqrt(2*np.pi)
 
+# Check that they are equal
+print("Analytic Corr = Calculated Corr?: ", np.allclose(analytic_corr_3D, corr_3D.real))
+print("Analytic Spec Dens = Calculated Spec Dens?: ", np.allclose(analytic_spec_dens_3D, spec_dens_3D.real))
 
-fig3 = plt.figure(3)
-ax3 = fig3.add_subplot(projection='3d')
-ax3.plot_wireframe(xx[:, :, 128], yy[:, :, 128], analytic_corr_3D[:, :, 128])
-ax3.set_xlabel("x")
-ax3.set_ylabel("y")
-ax3.set_zlabel("Corr")
-
-# Calculate power spectral density by calculating the fourier transform over each dimension
-temp = fft_3D(analytic_corr_3D, x_grid_dim, y_grid_dim, z_grid_dim, dx, dy, dz)
-pow_spec_dens_3D = minus * fft.fft(minus * analytic_corr_3D) * dx / (2 * np.pi)
-pow_spec_dens_3D = minus * fft.fft(minus * pow_spec_dens_3D) * dy / (2 * np.pi)
-pow_spec_dens_3D = minus * fft.fft(minus * pow_spec_dens_3D) * dz / (2 * np.pi)
-
-# Calculate analytical power spectral density
-k = np.sqrt(kkx ** 2 + kky ** 2 + kkz ** 2)
-analytic_spec_dens_3D = a ** 2 * r0 ** 3 / (np.pi ** 2 * (1 + r0 ** 2 * k ** 2) ** 2)
-print(np.allclose(temp.real / temp.real.max(), analytic_spec_dens_3D / analytic_spec_dens_3D.max()))
-fig4 = plt.figure(4)
-ax4 = fig4.add_subplot(projection='3d')
-ax4.plot_wireframe(kkx[:, :, 128], kky[:, :, 128], analytic_spec_dens_3D[:, :, 128])
-ax4.plot_wireframe(kkx[:, :, 128], kky[:, :, 128], temp[:, :, 128], color='orange')
-ax4.set_xlabel("kx")
-ax4.set_ylabel("ky")
-ax4.set_zlabel("phi(k)")
-"""
+fig5 = plt.figure(5)
+ax5 = fig5.add_subplot(projection='3d')
+ax5.plot_wireframe(Kx[:, :, 128], Ky[:, :, 128], analytic_spec_dens_3D[:, :, 128], label='Analytic')
+ax5.plot_wireframe(Kx[:, :, 128], Ky[:, :, 128], spec_dens_3D[:, :, 128], label='FFT', color='orange')
+ax5.set_xlabel("Kx")
+ax5.set_ylabel("Ky")
+ax5.set_zlabel("phi(kx, ky, kz=kz[128])")
+ax5.legend(numpoints=1)
 
 plt.show()
